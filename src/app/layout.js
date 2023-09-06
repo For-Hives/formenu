77fbcf636a1a3f25ef60acc8e1ac6e340e80108e
Cinquin Andy <andy.cinquin@infotel.com>
2023-09-06 17:57:49 +0200

    base style

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,5 @@
 import './styles/globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Head from "next/head";
 
 export const metadata = {
   title: 'Create Next App',
@@ -10,8 +8,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="fr">
+    <Head>
+      <link rel="stylesheet" href="https://use.typekit.net/npl8ujx.css"/>
+    </Head>
+      <body>{children}</body>
     </html>
   )
 }
