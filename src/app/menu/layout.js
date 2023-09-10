b9de06fe91984c1a2a4f25b4c96cb8c667fb4411
Cinquin Andy <andy.cinquin@infotel.com>
2023-09-10 15:28:41 +0200

    ✨ fonctionnalité(layout.js) : ajouter le logo du restaurant dans le menu layout

--- src/app/menu/layout.js
+++ src/app/menu/layout.js
@@ -1,7 +1,21 @@
+import Image from 'next/image'
+
 export default function Layout({ children }) {
 	return (
 		<>
-			<section className={'px-4'}>
+			<section className={'px-4 py-20'}>
+				<div className={'flex w-full justify-center gap-8'}>
+					<Image
+						src={'/icons/logo_restaurant.svg'}
+						width={50}
+						height={50}
+						alt={'logo_restaurant'}
+					/>
+					<div className={'flex flex-col gap-2'}>
+						<h1>Les pieds dans l'eau</h1>
+						<h2 className={'ml-4'}>traditionnel et authentique</h2>
+					</div>
+				</div>
 				<h2>Menu Layout</h2>
 				{children}
 			</section>
