5413c0da4b6382dc27d9e5bf28bc0e41138dd176
Cinquin Andy <andy.cinquin@infotel.com>
2023-09-15 23:54:18 +0200

    Refactor getData function to accept category instead of title
    - The `getData` function has been refactored to accept the `category` parameter instead of the `title` parameter.
    - The API URL in the fetch request has been updated to use the `category` parameter instead of the `title` parameter.
    - Added a new function called `getLastChildElements` that fetches data based on the `category`.
    - Updated the Page component to use the new parameters and functions.
    - Removed unnecessary console.log statements.
    
    This commit improves code readability and makes it more flexible by using category instead of title.

--- src/app/[category]/page.js
+++ src/app/[category]/page.js
@@ -5,10 +5,9 @@ import { Suspense } from 'react'
 import BackToPrevious from '@/components/BackToPrevious'
 import Link from 'next/link'
 
-async function getData(title) {
-	// todo change this to get the menu from the params 'slug' and not from the title
+async function getData(category) {
 	const res = await fetch(
-		`${process.env.NEXT_PUBLIC_API_URL}/api/categories?populate=deep&filters[menu][title][$eq]=${title}`,
+		`${process.env.NEXT_PUBLIC_API_URL}/api/categories?populate=deep&filters[category][id][$eq]=${category}`,
 		{
 			method: 'GET',
 			headers: {
@@ -28,65 +27,88 @@ async function getData(title) {
 	return res.json()
 }
 
-export default async function Page({ params }) {
-	const { menu } = params
-	console.log(menu)
-	const data = await getData(menu)
-	console.log(data)
-	return (
-		<>
-			<div className={'container-menus'}>
-				<Suspense fallback={<div>loading</div>}>
-					{data?.data.length > 0 ? (
-						<>
-							{/* ❌ loop on category if it's the first children category */}
-							{data?.data?.map((record, index) => {
-								return (
-									<Link
-										key={record.id}
-										className={'btn-alt-primary'}
-										href={`/${encodeURI(record.attributes.name.toString())}`}
-									>
-										{record.attributes.name}
-									</Link>
-								)
-							})}
-							{/* ✅ loop on category if it's the last children category */}
-							{data?.data?.map((record, index) => {
-								return (
-									<Link
-										key={record.id}
-										className={'btn-alt-primary'}
-										href={`/${encodeURI(record.attributes.name.toString())}`}
-									>
-										{record.attributes.name}
-									</Link>
-								)
-							})}
-							{/* ❓ loop on category if it's no't the last children category (other) */}
-							{data?.data?.map((record, index) => {
-								return (
-									<Link
-										key={record.id}
-										className={'btn-alt-primary'}
-										href={`/${encodeURI(record.attributes.name.toString())}`}
-									>
-										{record.attributes.name}
-									</Link>
-								)
-							})}
-						</>
-					) : (
-						// When there is no data
-						<div className={'flex flex-col gap-4 '}>
-							<p className={''}>{`Il n'y a rien dans ce menu !`}</p>
-							<div>
-								<BackToPrevious />
-							</div>
-						</div>
-					)}
-				</Suspense>
-			</div>
-		</>
+async function getLastChildElements(category) {
+	const res = await fetch(
+		`${process.env.NEXT_PUBLIC_API_URL}/api/categories?populate[categories][category][dishes]=deep&filters[categories][name][$notNull]=false&&filters[category][id]=${category}`,
+		{
+			method: 'GET',
+			headers: {
+				// 	token
+				'Content-Type': 'application/json',
+				Accept: 'application/json',
+				Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
+			},
+		}
 	)
+
+	if (!res.ok) {
+		throw new Error('Failed to fetch data')
+	}
+
+	return res.json()
+}
+
+export default async function Page({ params }) {
+	const { category } = params
+	const data = await getData(category)
+	const dataLastChild = await getLastChildElements(category)
+
+	if (data?.data.length)
+		// search in
+		return (
+			<>
+				<div className={'container-menus'}>
+					<Suspense fallback={<div>loading</div>}>
+						{data?.data.length > 0 ? (
+							<>
+								{/* ❌ loop on category if it's the first children category */}
+								{data?.data?.map((record, index) => {
+									return (
+										<Link
+											key={record.id}
+											className={'btn-alt-primary'}
+											href={`/${record.id.toString()}`}
+										>
+											{record.attributes.name}
+										</Link>
+									)
+								})}
+								{/* ✅ loop on category if it's the last children category */}
+								{/*{data?.data?.map((record, index) => {*/}
+								{/*	return (*/}
+								{/*		<Link*/}
+								{/*			key={record.id}*/}
+								{/*			className={'btn-alt-primary'}*/}
+								{/*			href={`/${encodeURI(record.attributes.name.toString())}`}*/}
+								{/*		>*/}
+								{/*			{record.attributes.name}*/}
+								{/*		</Link>*/}
+								{/*	)*/}
+								{/*})}*/}
+								{/* ❓ loop on category if it's no't the last children category (other) */}
+								{/*{data?.data?.map((record, index) => {*/}
+								{/*	return (*/}
+								{/*		<Link*/}
+								{/*			key={record.id}*/}
+								{/*			className={'btn-alt-primary'}*/}
+								{/*			href={`/${encodeURI(record.attributes.name.toString())}`}*/}
+								{/*		>*/}
+								{/*			{record.attributes.name}*/}
+								{/*		</Link>*/}
+								{/*	)*/}
+								{/*})}*/}
+							</>
+						) : (
+							// When there is no data
+							<div className={'flex flex-col gap-4 '}>
+								<p className={''}>{`Il n'y a rien dans ce menu !`}</p>
+								<div>
+									<BackToPrevious />
+								</div>
+							</div>
+						)}
+					</Suspense>
+				</div>
+			</>
+		)
 }
--- src/app/page.js
+++ src/app/page.js
@@ -40,7 +40,7 @@ export default async function Page() {
 						<Link
 							className={'btn-alt-primary'}
 							key={record.id}
-							href={`/${encodeURI(record.attributes.name.toString())}`}
+							href={record.id.toString()}
 						>
 							<Image
 								src={'/icons/menu_icon.svg'}
