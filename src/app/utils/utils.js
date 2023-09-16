6a0670d6a25eecbb825184a57963e2e801d43df3
Cinquin Andy <andy.cinquin@infotel.com>
2023-09-16 12:02:21 +0200

    Refactor createSlug function to include next_next and previous_previous IDs
    The createSlug function in utils.js has been modified to include the IDs of the two records before and after the current record. This change allows for more comprehensive navigation within the data.

--- src/app/utils/utils.js
+++ src/app/utils/utils.js
@@ -12,6 +12,8 @@ export function convertStringToKebabCase(str) {
 export function createSlug(data, record, index) {
 	const next = data?.data[index + 1]?.id.toString() ?? 'none'
 	const previous = data?.data[index - 1]?.id.toString() ?? 'none'
+	const previous_previous = data?.data[index - 2]?.id.toString() ?? 'none'
+	const next_next = data?.data[index + 2]?.id.toString() ?? 'none'
 	const current = record.id.toString()
-	return `/${current}?n=${next}&p=${previous}`
+	return `/${current}?n=${next}&p=${previous}&nn=${next_next}&pp=${previous_previous}`
 }
