9fe7fedf3deaebc7a8f02d81ca5de72cedf11bd8
CinquinAndy <cinquin.andy@gmail.com>
2023-09-29 14:27:05 +0200

    🔧 Refactor getData.js and ShoppingCartItemsList.js
    🔧 Refactor getData.js and ShoppingCartItemsList.js
    
    - In getData.js:
      - Reformat code for better readability
      - Update fetch URL to use process.env.NEXT_PUBLIC_API_URL and process.env.NEXT_PUBLIC_API_TOKEN
      - Throw an error if fetching data fails
    
    - In ShoppingCartItemsList.js:
      - Import getDishes and getDishFromId from getData.js
      - Use useEffect and useState hooks to fetch dishes data and store it in state
      - Display a loading spinner while fetching dishes data
      - Modify itemInfo function to find dish information based on its ID
      - Update rendering logic to display dish name and price instead of just item name

--- src/app/services/getData.js
+++ src/app/services/getData.js
@@ -91,3 +91,22 @@ export async function getNextCategoryInfos(current_category_data) {
 	})()
 	return next_category[0]
 }
+
+export async function getDishes() {
+	const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/dishes`, {
+		method: 'GET',
+		headers: {
+			// 	token
+			'Content-Type': 'application/json',
+			Accept: 'application/json',
+			Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
+		},
+	})
+
+	if (!res.ok) {
+		// This will activate the closest `error.js` Error Boundary
+		throw new Error('Failed to fetch data')
+	}
+
+	return res.json()
+}
--- src/components/ShoppingCartComponents/ShoppingCartItemsList.js
+++ src/components/ShoppingCartComponents/ShoppingCartItemsList.js
@@ -1,27 +1,52 @@
 'use client'
 import { useCart } from '@/app/providers/CartProvider'
+import { getDishes, getDishFromId } from '@/app/services/getData'
+import { useEffect, useState } from 'react'
+import { Spinner } from '@nextui-org/react'
 
 export function ShoppingCartItemsList() {
 	const { itemsInCart } = useCart()
+
+	const [dishes, setDishes] = useState([])
+
+	const itemInfo = item => {
+		console.log('item', item)
+		if (!dishes.length) return
+		return dishes.find(dish => dish.id.toString() === item.toString())
+	}
+
+	useEffect(() => {
+		getDishes().then(data => {
+			console.log('data', data)
+			setDishes(data)
+		})
+	}, [])
+
 	return (
 		<div className={'flex w-full flex-col gap-6'}>
-			{itemsInCart?.length ? (
-				itemsInCart.map((item, index) => {
-					return (
-						<div
-							key={index}
-							className={'flex w-full items-center justify-start'}
-						>
-							<p>
-								{item.name} - {item.price}
-							</p>
+			{dishes?.length ? (
+				<>
+					{itemsInCart?.length ? (
+						itemsInCart.map((item, index) => {
+							return (
+								<div
+									key={index}
+									className={'flex w-full items-center justify-start'}
+								>
+									<p>{console.log('item', itemInfo(item))}</p>
+								</div>
+							)
+						})
+					) : (
+						<div className={'flex w-full items-center justify-start'}>
+							<p>No items in cart</p>
 						</div>
-					)
-				})
+					)}
+				</>
 			) : (
-				<div className={'flex w-full items-center justify-start'}>
-					<p>No items in cart</p>
-				</div>
+				<>
+					<Spinner />
+				</>
 			)}
 		</div>
 	)
