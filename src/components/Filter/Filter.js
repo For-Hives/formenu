77f806c1b5f6afbdcdad766556731db2c07951e2
Cinquin Andy <andy.cinquin@infotel.com>
2023-10-01 15:50:31 +0200

    🔧 Update Filter component styling and structure
    The commit updates the styling and structure of the Filter component. Changes include:
    - Adjusting flex layout and alignment
    - Adding shadow to input field
    - Modifying placeholder text size
    - Updating position and background color of magnifying glass icon
    - Adding a new section for filtering options with appropriate styling

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -8,22 +8,49 @@ function Filter(props) {
 				'fixed left-0 top-0 z-50 flex h-screen w-screen items-start justify-start bg-black/10 py-4'
 			}
 		>
-			<div className={'flex w-full items-center justify-end'}>
+			<div
+				className={'ml-4 flex w-full flex-col items-end justify-center gap-4'}
+			>
 				<div
-					className={'relative flex w-full max-w-lg items-center justify-end'}
+					className={
+						'relative flex w-full max-w-lg items-center justify-end shadow-xl'
+					}
 				>
 					<input
 						className={
-							'ml-4 flex w-full items-center rounded-l-lg bg-slate-50 py-3 pl-12 shadow-xl'
+							'flex w-full items-center rounded-l-lg bg-slate-50 py-3 pl-12 text-sm'
 						}
 						placeholder={'Rechercher un plat avec un mot clé...'}
 					></input>
 					<CustomSvg
 						url={'/icons/magnifying-glass.svg'}
 						classNames={
-							'ml-9 absolute top-1/2 transform -translate-y-1/2 left-0 h-[15px] w-[15px] bg-black'
+							'ml-5 absolute top-1/2 transform -translate-y-1/2 left-0 h-[15px] w-[15px] bg-black'
 						}
-					></CustomSvg>
+					/>
+				</div>
+				<div
+					className={
+						'relative flex w-full max-w-lg items-center justify-end shadow-xl'
+					}
+				>
+					<div
+						className={
+							'flex h-full w-full flex-col gap-8 rounded-l-lg border border-blue-950 bg-slate-50 py-3'
+						}
+					>
+						<div className={'flex gap-4 border-b border-b-slate-200 pb-3 pl-4'}>
+							<div className={'flex items-center justify-center'}>
+								<CustomSvg
+									url={'/icons/filter.svg'}
+									classNames={'h-[15px] w-[15px] bg-black'}
+								/>
+							</div>
+							<p className={'text-sm font-bold'}>Filtrer</p>
+						</div>
+						<div>Regime Alimentaire</div>
+						<div>Allergènes</div>
+					</div>
 				</div>
 			</div>
 		</div>
