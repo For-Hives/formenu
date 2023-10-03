b2fb7a8ee91100135965a6f6b4ff58bba49ee886
Cinquin Andy <andy.cinquin@infotel.com>
2023-10-03 17:18:15 +0200

    🔧 Refactor DietFilter component
    - Extracted the repeated JSX code into a separate div element
    - Added key prop to the div element for better performance and rendering
    - Improved readability by formatting the JSX code

--- src/components/Filter/DietFilter.js
+++ src/components/Filter/DietFilter.js
@@ -41,27 +41,25 @@ export function DietFilter() {
 	return (
 		<div className={'grid w-full grid-cols-12 gap-2 px-8'}>
 			{diets.map(diet => (
-				<>
-					<div
-						key={diet.key}
-						className={
-							'col-span-3 flex flex-col items-center justify-center gap-2'
-						}
+				<div
+					key={diet.key}
+					className={
+						'col-span-3 flex flex-col items-center justify-center gap-2'
+					}
+				>
+					<DietButton
+						diet={diet}
+						selectedDiet={selectedDiet}
+						onDietChange={setSelectedDiet}
+					/>
+					<p
+						className={`text-xs text-slate-600 ${
+							selectedDiet === diet.key && 'underline'
+						}`}
 					>
-						<DietButton
-							diet={diet}
-							selectedDiet={selectedDiet}
-							onDietChange={setSelectedDiet}
-						/>
-						<p
-							className={`text-xs text-slate-600 ${
-								selectedDiet === diet.key && 'underline'
-							}`}
-						>
-							{diet.key.charAt(0).toUpperCase() + diet.key.slice(1)}
-						</p>
-					</div>
-				</>
+						{diet.key.charAt(0).toUpperCase() + diet.key.slice(1)}
+					</p>
+				</div>
 			))}
 		</div>
 	)
