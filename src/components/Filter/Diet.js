9e88ace8e77a83ecd21463631c2097713ec44d57
Cinquin Andy <andy.cinquin@infotel.com>
2023-10-03 16:11:08 +0200

    🔧 Update Diet component and global styles
    - Added 'use client' import to Diet.js
    - Refactored Diet component to use DietButton subcomponent
    - Implemented dynamic rendering of diet buttons based on selectedDiet state
    - Added onClick event handler for diet button selection
    - Updated global CSS styles for diet buttons
    
    This commit improves the functionality and appearance of the Diet component by introducing a reusable subcomponent, implementing dynamic rendering of diet buttons, and updating the global CSS styles for better visual representation.

--- src/components/Filter/Diet.js
+++ src/components/Filter/Diet.js
@@ -1,36 +1,61 @@
+'use client'
 import Image from 'next/image'
+import { useState } from 'react'
+
+function DietButton({ diet, selectedDiet, onDietChange }) {
+	const isSelected = diet.type === selectedDiet
+	return (
+		<>
+			<button
+				className={`${
+					isSelected ? 'diet-button-selected' : 'diet-button'
+				} relative`}
+				onClick={() => onDietChange(diet.type)}
+			>
+				{diet.icon ? (
+					<Image src={diet.icon} width={20} height={20} alt={diet.type} />
+				) : (
+					<span className={'sr-only'}>{diet.type}</span>
+				)}
+
+				<span className={'absolute right-0 top-0 m-1'}>
+					<div
+						className={
+							'flex items-center justify-center rounded-full border border-slate-300 bg-slate-50 p-0.5'
+						}
+					>
+						{isSelected ? (
+							<div className={'h-1 w-1 rounded-full bg-black/75'}></div>
+						) : (
+							<div className={'h-1 w-1 rounded-full bg-transparent'}></div>
+						)}
+					</div>
+				</span>
+			</button>
+		</>
+	)
+}
 
 export function Diet() {
+	const [selectedDiet, setSelectedDiet] = useState('default')
+
+	const diets = [
+		{ type: 'omnivore', icon: '/icons/omnivore.svg' },
+		{ type: 'vegetarien', icon: '/icons/vege.svg' },
+		{ type: 'vegan', icon: '/icons/vegan.svg' },
+		{ type: 'default' },
+	]
+
 	return (
 		<div className={'grid w-full grid-cols-12 gap-2 px-8'}>
-			<button
-				className={
-					'col-span-3 flex items-center justify-center rounded-lg bg-slate-100 py-4'
-				}
-			>
-				<Image src={'/icons/Fish.svg'} width={20} height={20} alt={'elem'} />
-			</button>
-			<button
-				className={
-					'col-span-3 flex items-center justify-center rounded-lg bg-slate-100 py-4'
-				}
-			>
-				<Image src={'/icons/Fish.svg'} width={20} height={20} alt={'elem'} />
-			</button>
-			<button
-				className={
-					'col-span-3 flex items-center justify-center rounded-lg bg-slate-100 py-4'
-				}
-			>
-				<Image src={'/icons/Fish.svg'} width={20} height={20} alt={'elem'} />
-			</button>
-			<button
-				className={
-					'col-span-3 flex items-center justify-center rounded-lg bg-slate-100 py-4'
-				}
-			>
-				<Image src={'/icons/Fish.svg'} width={20} height={20} alt={'elem'} />
-			</button>
+			{diets.map(diet => (
+				<DietButton
+					key={diet.type}
+					diet={diet}
+					selectedDiet={selectedDiet}
+					onDietChange={setSelectedDiet}
+				/>
+			))}
 		</div>
 	)
 }
