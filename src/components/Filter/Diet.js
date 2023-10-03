a93a95286753c9aa124e259510c9ba9ca4939165
Cinquin Andy <andy.cinquin@infotel.com>
2023-10-03 16:18:38 +0200

    🔧 Update DietButton component and styling
    - Updated the DietButton component to remove unnecessary wrapping elements.
    - Adjusted the styling of the DietButton component to make it occupy the full width and height.
    - Added a new paragraph element to display the capitalized diet type below each button.
    
    This commit improves the code structure and enhances the visual appearance of the DietButton component.

--- src/components/Filter/Diet.js
+++ src/components/Filter/Diet.js
@@ -5,34 +5,32 @@ import { useState } from 'react'
 function DietButton({ diet, selectedDiet, onDietChange }) {
 	const isSelected = diet.type === selectedDiet
 	return (
-		<>
-			<button
-				className={`${
-					isSelected ? 'diet-button-selected' : 'diet-button'
-				} relative`}
-				onClick={() => onDietChange(diet.type)}
-			>
-				{diet.icon ? (
-					<Image src={diet.icon} width={20} height={20} alt={diet.type} />
-				) : (
-					<span className={'sr-only'}>{diet.type}</span>
-				)}
+		<button
+			className={`${
+				isSelected ? 'diet-button-selected' : 'diet-button'
+			} relative`}
+			onClick={() => onDietChange(diet.type)}
+		>
+			{diet.icon ? (
+				<Image src={diet.icon} width={20} height={20} alt={diet.type} />
+			) : (
+				<span className={'sr-only'}>{diet.type}</span>
+			)}
 
-				<span className={'absolute right-0 top-0 m-1'}>
-					<div
-						className={
-							'flex items-center justify-center rounded-full border border-slate-300 bg-slate-50 p-0.5'
-						}
-					>
-						{isSelected ? (
-							<div className={'h-1 w-1 rounded-full bg-black/75'}></div>
-						) : (
-							<div className={'h-1 w-1 rounded-full bg-transparent'}></div>
-						)}
-					</div>
-				</span>
-			</button>
-		</>
+			<span className={'absolute right-0 top-0 m-1'}>
+				<div
+					className={
+						'flex items-center justify-center rounded-full border border-slate-300 bg-slate-50 p-0.5'
+					}
+				>
+					{isSelected ? (
+						<div className={'h-1 w-1 rounded-full bg-black/75'}></div>
+					) : (
+						<div className={'h-1 w-1 rounded-full bg-transparent'}></div>
+					)}
+				</div>
+			</span>
+		</button>
 	)
 }
 
@@ -49,12 +47,27 @@ export function Diet() {
 	return (
 		<div className={'grid w-full grid-cols-12 gap-2 px-8'}>
 			{diets.map(diet => (
-				<DietButton
-					key={diet.type}
-					diet={diet}
-					selectedDiet={selectedDiet}
-					onDietChange={setSelectedDiet}
-				/>
+				<>
+					<div
+						className={
+							'col-span-3 flex flex-col items-center justify-center gap-2'
+						}
+					>
+						<DietButton
+							key={diet.type}
+							diet={diet}
+							selectedDiet={selectedDiet}
+							onDietChange={setSelectedDiet}
+						/>
+						<p
+							className={`text-xs text-slate-600 ${
+								selectedDiet === diet.type && 'underline'
+							}`}
+						>
+							{diet.type.charAt(0).toUpperCase() + diet.type.slice(1)}
+						</p>
+					</div>
+				</>
 			))}
 		</div>
 	)
