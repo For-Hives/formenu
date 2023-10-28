a7cca1f0417585e207850bead09fc5a3514f7fbd
Cinquin Andy <andy.cinquin@infotel.com>
2023-10-28 21:33:23 +0200

    🎨 Update Sort.js component
    Refactored the sorting options in the Sort.js component to improve readability and user experience. Updated button labels for better clarity and added default highlighting for the "Mis en avant" option.

--- src/components/Filter/Sort.js
+++ src/components/Filter/Sort.js
@@ -34,59 +34,84 @@ export function Sort() {
 							leaveFrom="transform opacity-100 scale-100"
 							leaveTo="transform opacity-0 scale-95"
 						>
-							<Menu.Items className="absolute left-0 mt-2 w-56 origin-top-left divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
-								<div className="px-1 py-1">
-									<Menu.Item>
-										{({ active }) => (
-											<button
-												className={`${
-													active ? 'bg-blue-500 text-white' : 'text-gray-900'
-												} group flex w-full items-center gap-3 rounded-md px-2 py-2 text-sm`}
-											>
-												Mis en avant
-											</button>
-										)}
-									</Menu.Item>
-								</div>
-								<div className="px-1 py-1">
-									<Menu.Item>
-										{({ active }) => (
-											<button
-												className={`${
-													active ? 'bg-blue-500 text-white' : 'text-gray-900'
-												} group flex w-full items-center gap-3 rounded-md px-2 py-2 text-sm`}
-											>
-												Prix : Ordre croissant
-											</button>
-										)}
-									</Menu.Item>
-								</div>
-								<div className="px-1 py-1">
-									<Menu.Item>
-										{({ active }) => (
-											<button
-												className={`${
-													active ? 'bg-blue-500 text-white' : 'text-gray-900'
-												} group flex w-full items-center gap-3 rounded-md px-2 py-2 text-sm`}
-											>
-												Prix : Ordre décroissant
-											</button>
-										)}
-									</Menu.Item>
-								</div>
-								<div className="px-1 py-1">
-									<Menu.Item>
-										{({ active }) => (
-											<button
-												className={`${
-													active ? 'bg-blue-500 text-white' : 'text-gray-900'
-												} group flex w-full items-center gap-3 rounded-md px-2 py-2 text-sm`}
-											>
-												Ordre alphabetique
-											</button>
-										)}
-									</Menu.Item>
-								</div>
+							<Menu.Items className="absolute left-0 mt-2 w-56 origin-top-left divide-y divide-gray-100 rounded-md bg-white py-2 shadow-lg ring-1 ring-black/5 focus:outline-none">
+								<Menu.Item>
+									{({ active }) => (
+										<button
+											className={`${
+												active ? 'bg-blue-950 text-white' : 'text-gray-900'
+											} flex w-full items-center gap-3 px-2 py-2 text-sm`}
+										>
+											Mis en avant (par défaut)
+										</button>
+									)}
+								</Menu.Item>
+								<Menu.Item>
+									{({ active }) => (
+										<button
+											className={`${
+												active ? 'bg-blue-950 text-white' : 'text-gray-900'
+											} flex w-full items-center gap-3 px-2 py-2 text-sm`}
+										>
+											Prix : Ordre croissant
+										</button>
+									)}
+								</Menu.Item>
+								<Menu.Item>
+									{({ active }) => (
+										<button
+											className={`${
+												active ? 'bg-blue-950 text-white' : 'text-gray-900'
+											} flex w-full items-center gap-3 px-2 py-2 text-sm`}
+										>
+											Prix : Ordre décroissant
+										</button>
+									)}
+								</Menu.Item>
+								<Menu.Item>
+									{({ active }) => (
+										<button
+											className={`${
+												active ? 'bg-blue-950 text-white' : 'text-gray-900'
+											} flex w-full items-center gap-3 px-2 py-2 text-sm`}
+										>
+											Ordre alphabetique : A-Z
+										</button>
+									)}
+								</Menu.Item>
+								<Menu.Item>
+									{({ active }) => (
+										<button
+											className={`${
+												active ? 'bg-blue-950 text-white' : 'text-gray-900'
+											} flex w-full items-center gap-3 px-2 py-2 text-sm`}
+										>
+											Ordre alphabetique : Z-A
+										</button>
+									)}
+								</Menu.Item>
+								<Menu.Item>
+									{({ active }) => (
+										<button
+											className={`${
+												active ? 'bg-blue-950 text-white' : 'text-gray-900'
+											} flex w-full items-center gap-3 px-2 py-2 text-sm`}
+										>
+											{`Date d'ajout : Ordre croissant`}
+										</button>
+									)}
+								</Menu.Item>
+								<Menu.Item>
+									{({ active }) => (
+										<button
+											className={`${
+												active ? 'bg-blue-950 text-white' : 'text-gray-900'
+											} flex w-full items-center gap-3 px-2 py-2 text-sm`}
+										>
+											{`Date d'ajout : Ordre décroissant`}
+										</button>
+									)}
+								</Menu.Item>
 							</Menu.Items>
 						</Transition>
 					</Menu>
