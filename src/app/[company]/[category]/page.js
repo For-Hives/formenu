4ddf3e848c7c374643ab2afaba24944f9ac4015f
Cinquin Andy <andy.cinquin@infotel.com>
2023-11-01 16:42:59 +0100

    🔧 Add SkeletonContainer component for loading state
    This commit adds a new file `SkeletonContainer.js` which contains the `SkeletonContainer` component. This component is used to display a loading state with skeleton placeholders while data is being fetched. It includes various skeleton elements such as buttons and icons.
    
    The `SkeletonContainer` component is imported and used in the `Page` function of the `page.js` file. When the flag variable is true, the loading state is displayed using the `Suspense` component and the `SkeletonContainer`.

--- src/app/[company]/[category]/page.js
+++ src/app/[company]/[category]/page.js
@@ -10,6 +10,9 @@ import { Nav } from '@/components/Layout/Nav'
 import { CustomSvg } from '@/components/CustomSvg'
 import { DishListStaticOrDynamic } from '@/components/Dishes/DishListStaticOrDynamic'
 import DishesListStatic from '@/components/Dishes/DishesListStatic'
+import SkeletonDish from '@/components/Loaders/SkeletonDish'
+import { Suspense } from 'react'
+import { SkeletonContainer } from '@/components/Loaders/SkeletonContainer'
 
 export default async function Page({ params }) {
 	const { category, company } = params
@@ -29,123 +32,138 @@ export default async function Page({ params }) {
 		company
 	)
 
+	const flag = true
 	return (
 		<>
-			<Nav
-				parent_categories={parent_categories}
-				selected_category={current_category_data?.id}
-				company_slug={company}
-				company={company}
-				category={category}
-			/>
-			<div className={'container-menu'}>
-				<div
-					className={`${
-						data ? 'min-h-[calc(100vh-25rem)]' : ''
-					} container-sub-menus`}
-				>
-					{
-						// ✅ get the previous parent category, url
-						// from search params, get the previous parent category
-						current_category_data?.categories.length > 0 &&
-							previous_category_data && (
-								<div className={'flex w-full items-center justify-start'}>
-									<Link
-										className={'btn-primary'}
-										href={`/${company}/${previous_category_data?.id.toString()}`}
-									>
-										<CustomSvg
-											url={previous_category_data.icon.url}
-											classNames={'bg-white'}
-										/>
-										{previous_category_data?.name}
-									</Link>
-								</div>
-							)
-					}
-					{
-						// ✅ get the parent category, url
-						<div className={'flex w-full items-center justify-start'}>
-							<Link
-								className={'btn-primary'}
-								href={`/${company}/${current_category_data?.id.toString()}`}
-							>
-								<CustomSvg
-									url={current_category_data?.icon?.url}
-									classNames={'bg-white'}
-								/>
-								{current_category_data?.name}
-							</Link>
+			{flag ? (
+				<>
+					<SkeletonContainer isLoaded={false} />
+				</>
+			) : (
+				<Suspense
+					fallback={
+						<div className={`container-dishes`}>
+							<SkeletonDish />
 						</div>
 					}
-					{current_category_data?.categories.length > 0 ? (
-						<>
-							{/* ❌ loop on category if it's the first children category */}
-							{data.categories.map((record, index) => {
-								return (
-									<div
-										key={record.id}
-										className={'flex w-full items-center justify-end'}
-									>
-										<Link
-											className={'btn-alt-primary'}
-											href={`/${company}/${record.id.toString()}`}
-										>
-											<CustomSvg
-												url={record.icon.url}
-												classNames={'bg-blue-950'}
-											/>
-											{record.name}
-										</Link>
-									</div>
-								)
-							})}
-						</>
-					) : (
-						// When there is no children categories -> display dishes
-						<>
-							<DishListStaticOrDynamic
-								category={category}
-								company={company}
-								data={data}
-								DishesListStatic={
-									<DishesListStatic category={category} company={company} />
-								}
-							/>
-
-							{next_category_data && (
+				>
+					<Nav
+						parent_categories={parent_categories}
+						selected_category={current_category_data?.id}
+						company_slug={company}
+						company={company}
+						category={category}
+					/>
+					<div className={'container-menu'}>
+						<div
+							className={`${
+								data ? 'min-h-[calc(100vh-25rem)]' : ''
+							} container-sub-menus`}
+						>
+							{
+								// ✅ get the previous parent category, url
+								// from search params, get the previous parent category
+								current_category_data?.categories.length > 0 &&
+									previous_category_data && (
+										<div className={'flex w-full items-center justify-start'}>
+											<Link
+												className={'btn-primary'}
+												href={`/${company}/${previous_category_data?.id.toString()}`}
+											>
+												<CustomSvg
+													url={previous_category_data.icon.url}
+													classNames={'bg-white'}
+												/>
+												{previous_category_data?.name}
+											</Link>
+										</div>
+									)
+							}
+							{
+								// ✅ get the parent category, url
 								<div className={'flex w-full items-center justify-start'}>
 									<Link
 										className={'btn-primary'}
-										href={`/${company}/${next_category_data?.id.toString()}`}
-									>
-										→&nbsp;Voir les {next_category_data?.name}
-									</Link>
-								</div>
-							)}
-						</>
-					)}
-					{
-						// ✅ get the next parent category
-						// from search params, get the next parent category
-						current_category_data?.categories.length > 0 &&
-							next_category_data && (
-								<div className={'flex w-full items-center justify-start'}>
-									<Link
-										className={'btn-primary'}
-										href={`/${company}/${next_category_data?.id.toString()}`}
+										href={`/${company}/${current_category_data?.id.toString()}`}
 									>
 										<CustomSvg
-											url={next_category_data.icon.url}
+											url={current_category_data?.icon?.url}
 											classNames={'bg-white'}
 										/>
-										{next_category_data?.name}
+										{current_category_data?.name}
 									</Link>
 								</div>
-							)
-					}
-				</div>
-			</div>
+							}
+							{current_category_data?.categories.length > 0 ? (
+								<>
+									{/* ❌ loop on category if it's the first children category */}
+									{data.categories.map((record, index) => {
+										return (
+											<div
+												key={record.id}
+												className={'flex w-full items-center justify-end'}
+											>
+												<Link
+													className={'btn-alt-primary'}
+													href={`/${company}/${record.id.toString()}`}
+												>
+													<CustomSvg
+														url={record.icon.url}
+														classNames={'bg-blue-950'}
+													/>
+													{record.name}
+												</Link>
+											</div>
+										)
+									})}
+								</>
+							) : (
+								// When there is no children categories -> display dishes
+								<>
+									<DishListStaticOrDynamic
+										category={category}
+										company={company}
+										data={data}
+										DishesListStatic={
+											<DishesListStatic category={category} company={company} />
+										}
+									/>
+
+									{next_category_data && (
+										<div className={'flex w-full items-center justify-start'}>
+											<Link
+												className={'btn-primary'}
+												href={`/${company}/${next_category_data?.id.toString()}`}
+											>
+												→&nbsp;Voir les {next_category_data?.name}
+											</Link>
+										</div>
+									)}
+								</>
+							)}
+							{
+								// ✅ get the next parent category
+								// from search params, get the next parent category
+								current_category_data?.categories.length > 0 &&
+									next_category_data && (
+										<div className={'flex w-full items-center justify-start'}>
+											<Link
+												className={'btn-primary'}
+												href={`/${company}/${next_category_data?.id.toString()}`}
+											>
+												<CustomSvg
+													url={next_category_data.icon.url}
+													classNames={'bg-white'}
+												/>
+												{next_category_data?.name}
+											</Link>
+										</div>
+									)
+							}
+						</div>
+					</div>
+				</Suspense>
+			)}
 		</>
 	)
 }
---
+++ src/components/Loaders/SkeletonContainer.js
@@ -0,0 +1,100 @@
+import Image from 'next/image'
+import { Skeleton } from '@nextui-org/react'
+
+export function SkeletonContainer(props) {
+	return (
+		<>
+			<div
+				className={
+					'pointer-events-none fixed left-0 top-0 z-30 h-screen w-screen select-none pb-8 md:pb-0'
+				}
+			>
+				<div className={'grid h-full w-full grid-cols-2'}>
+					<div
+						className={
+							'col-span-1 flex items-start justify-start gap-2 p-4 sm:gap-4 md:p-8'
+						}
+					>
+						<Skeleton className={'btn-nav'} />
+						<Skeleton className={'btn-nav'} />
+					</div>
+					<div
+						className={
+							'relative col-span-1 flex flex-col items-end justify-between px-4 pt-4 md:px-8 md:pt-8'
+						}
+					>
+						{/* start ------------------------------ underline decoration ------------------------------  */}
+						<div
+							className={
+								'absolute right-0 top-0 mx-4 flex h-full justify-center md:mx-8'
+							}
+						>
+							<div className={'flex h-full w-[40px] justify-center'}>
+								<div className={'relative h-full w-0.5 bg-slate-950'}></div>
+							</div>
+						</div>
+						{/* end ------------------------------ underline decoration ------------------------------  */}
+						<div
+							className={
+								'z-30 flex h-2/6 items-start justify-center gap-2 md:gap-4'
+							}
+						>
+							<Skeleton className={'h-[25px] w-[25px] rounded-full'} />
+							<Skeleton className={'h-[25px] w-[25px] rounded-full'} />
+							<Skeleton className={'h-[25px] w-[25px] rounded-full'} />
+							{/*<ShoppingCartButtonClear />*/}
+							{/*<ShoppingCartButtonResume company_slug={company_slug} />*/}
+
+							{/*<ButtonFilter />*/}
+						</div>
+
+						<div
+							className={
+								'relative flex h-4/6 w-full flex-col items-start justify-start gap-8 md:gap-16'
+							}
+						>
+							<div
+								className={
+									'absolute right-0 top-0 flex w-[40px] flex-col items-center justify-start'
+								}
+							>
+								<div
+									style={{
+										height: '100%',
+									}}
+									className={'z-0 w-0.5 bg-slate-950'}
+								></div>
+								<div className={'z-10 h-screen w-0.5 bg-slate-200'}></div>
+							</div>
+							<>
+								{/* ❌ loop on category if it's the first children category */}
+								<div className={'flex w-full items-center justify-end'}>
+									<Skeleton className={'h-[25px] w-[25px] rounded-full'} />
+									{/*<Link*/}
+									{/*	className={`${*/}
+									{/*		selected_category.toString() ===*/}
+									{/*		record.id.toString()*/}
+									{/*			? 'btn-nav-alt'*/}
+									{/*			: 'btn-nav'*/}
+									{/*	}`}*/}
+									{/*	href={`/${company_slug}/${record.id.toString()}`}*/}
+									{/*>*/}
+									{/*	<CustomSvg*/}
+									{/*		url={record.icon.url}*/}
+									{/*		classNames={*/}
+									{/*			selected_category.toString() ===*/}
+									{/*			record.id.toString()*/}
+									{/*				? 'bg-white'*/}
+									{/*				: 'bg-black'*/}
+									{/*		}*/}
+									{/*	/>*/}
+									{/*</Link>*/}
+								</div>
+							</>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	)
+}
