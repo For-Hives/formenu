35997a65361292854bd9c6569458a4a7db96a87f
Cinquin Andy <andy.cinquin@infotel.com>
2023-11-08 00:14:03 +0100

    🔧 Refactor useStore.js: Add recursive functions for checking diet and allergens
    This commit refactors the `useStore.js` file by adding two new recursive functions: `checkDietRecursive` and `checkAllergensRecursive`. These functions are used to check the diet and allergens for a given dish, respectively. The existing `checkDiet` and `checkAllergens` methods now call these new recursive functions. This change improves code readability and reduces duplication of logic.

--- src/providers/useStore.js
+++ src/providers/useStore.js
@@ -1,6 +1,46 @@
 import { create } from 'zustand'
 import { optionsSortData } from '@/enum/optionsSortData'
 
+const checkDietRecursive = (dish, diet) => {
+	// Check the diet for the current dish
+	const isDietMatch =
+		(diet === 'vegetarian' && dish.is_vegetarian) ||
+		(diet === 'vegan' && dish.is_vegan) ||
+		diet === 'default' ||
+		diet === 'omnivore'
+
+	// If the diet does not match, return false
+	if (!isDietMatch) return false
+
+	// If the dish has subdishes, check the diet recursively for each subdish
+	if (dish.dishes) {
+		return dish.dishes.every(subdish => checkDietRecursive(subdish, diet))
+	}
+
+	// If there are no subdishes or if all subdishes match the diet, return true
+	return true
+}
+
+const checkAllergensRecursive = (dish, selectedAllergens) => {
+	// Check allergens for the current dish
+	const hasAllergen = selectedAllergens.some(
+		allergen => dish?.allergens[allergen]
+	)
+
+	// If the dish has an allergen, return false
+	if (hasAllergen) return false
+
+	// If the dish has subdishes, check allergens recursively for each subdish
+	if (dish.dishes) {
+		return dish.dishes.every(subdish =>
+			checkAllergensRecursive(subdish, selectedAllergens)
+		)
+	}
+
+	// If there are no subdishes or if no subdishes have the allergens, return true
+	return true
+}
+
 export const useStore = create(set => ({
 	data: [],
 	isFilterModalClosed: true,
@@ -54,21 +94,11 @@ export const useStore = create(set => ({
 	},
 
 	checkDiet: (dish, diet) => {
-		switch (diet) {
-			case 'default':
-			case 'omnivore':
-				return true
-			case 'vegetarian':
-				return dish.is_vegetarian
-			case 'vegan':
-				return dish.is_vegan
-			default:
-				return true
-		}
+		return checkDietRecursive(dish, diet)
 	},
 
 	checkAllergens: (dish, selectedAllergens) => {
 		if (selectedAllergens?.length === 0) return true
-		return !selectedAllergens.some(allergen => dish?.allergens[allergen])
+		return checkAllergensRecursive(dish, selectedAllergens)
 	},
 }))
