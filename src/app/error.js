2fdb72a732c747409729b7561b073eb44271e85c
Cinquin Andy <andy.cinquin@infotel.com>
2023-11-09 20:49:53 +0100

    errors.js & background for error

---
+++ src/app/error.js
@@ -0,0 +1,42 @@
+'use client' // Error components must be Client Components
+
+import { useEffect } from 'react'
+import Image from 'next/image'
+
+export default function Error({ error, reset }) {
+	useEffect(() => {
+		// Log the error to an error reporting service
+		console.error(error)
+	}, [error])
+
+	return (
+		<main className="relative isolate min-h-screen">
+			<Image
+				src="https://images.unsplash.com/photo-1545972154-9bb223aac798?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3050&q=80&exp=8&con=-15&sat=-75"
+				alt="error"
+				fill={true}
+				className="absolute inset-0 -z-10 h-full w-full object-cover object-top"
+			/>
+			<div className="mx-auto max-w-7xl px-6 py-32 text-center sm:py-40 lg:px-8">
+				<p className="text-base font-semibold leading-8 text-white">404</p>
+				<h1 className="mt-4 text-3xl font-bold tracking-tight text-white sm:text-5xl">
+					Page not found
+				</h1>
+				<p className="mt-4 text-base text-white/70 sm:mt-6">
+					Sorry, we couldn’t find the page you’re looking for.
+				</p>
+				<div className="mt-10 flex justify-center">
+					<button
+						className="text-sm font-semibold leading-7 text-white"
+						onClick={
+							// Attempt to recover by trying to re-render the segment
+							() => reset()
+						}
+					>
+						<span aria-hidden="true">&larr;</span> Back to home
+					</button>
+				</div>
+			</div>
+		</main>
+	)
+}
