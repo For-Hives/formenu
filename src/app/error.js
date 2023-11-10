d2ab3d292791dee443d381db052f473727d29468
Cinquin Andy <andy.cinquin@infotel.com>
2023-11-10 18:06:06 +0100

    🎨 Update error page styling
    - Added a dark overlay to the background image
    - Adjusted opacity of the background image for better visibility
    - Updated text styling and added italics for emphasis
    - Added shadow to text for better readability

--- src/app/error.js
+++ src/app/error.js
@@ -10,22 +10,26 @@ export default function Error({ error, reset }) {
 	}, [error])
 
 	return (
-		<main className="relative isolate min-h-screen">
+		<main className="relative isolate z-30 min-h-screen">
+			{/* darken */}
+			<div className={'absolute left-0 top-0 -z-10 h-full w-full bg-black'} />
 			<Image
 				src="/error_background.png"
 				alt="error"
 				fill={true}
-				className="absolute inset-0 -z-10 h-full w-full object-cover object-left"
+				className="absolute inset-0 -z-10 h-full w-full object-cover object-left opacity-50"
 			/>
-			<div className="mx-auto max-w-7xl px-6 py-32 text-center sm:py-40 lg:px-8">
-				<p className="text-base font-semibold leading-8 text-white">erreur</p>
+			<div className="z-30 mx-auto max-w-7xl px-6 py-32 text-center sm:py-40 lg:px-8">
+				<p className="text-base font-semibold italic leading-8 text-white">
+					Oups !
+				</p>
 				<h1 className="mt-4 text-3xl font-bold tracking-tight text-white sm:text-5xl">
 					Une erreur est survenue
 				</h1>
-				<p className="mt-4 text-base text-white/70 sm:mt-6">
+				<p className="mt-4 text-base text-white shadow-lg sm:mt-6">
 					Désolé, une erreur est survenue. Veuillez réessayer.
 				</p>
-				<p className={'text-white/70'}>
+				<p className={'text-white shadow-lg'}>
 					Ou contactez-nous si le problème persiste.
 				</p>
 				<div className="mt-10 flex justify-center">
