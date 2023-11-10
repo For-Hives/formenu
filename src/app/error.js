7856ac860e281a1821ad56482e4a49e8c5570412
Cinquin Andy <andy.cinquin@infotel.com>
2023-11-10 17:43:01 +0100

    🔧 Update error page UI
    - Updated the background image source to use a local file instead of a remote URL.
    - Adjusted the object positioning of the background image.
    - Changed the text content from English to French.
    - Modified the error code from "404" to "erreur".
    - Translated the heading and error message into French.
    - Added an additional message in French for contacting support if the issue persists.
    
    ℹ️ Please review and test these changes thoroughly.

--- src/app/error.js
+++ src/app/error.js
@@ -12,18 +12,21 @@ export default function Error({ error, reset }) {
 	return (
 		<main className="relative isolate min-h-screen">
 			<Image
-				src="https://images.unsplash.com/photo-1545972154-9bb223aac798?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3050&q=80&exp=8&con=-15&sat=-75"
+				src="/error_background.png"
 				alt="error"
 				fill={true}
-				className="absolute inset-0 -z-10 h-full w-full object-cover object-top"
+				className="absolute inset-0 -z-10 h-full w-full object-cover object-left"
 			/>
 			<div className="mx-auto max-w-7xl px-6 py-32 text-center sm:py-40 lg:px-8">
-				<p className="text-base font-semibold leading-8 text-white">404</p>
+				<p className="text-base font-semibold leading-8 text-white">erreur</p>
 				<h1 className="mt-4 text-3xl font-bold tracking-tight text-white sm:text-5xl">
-					Page not found
+					Une erreur est survenue
 				</h1>
 				<p className="mt-4 text-base text-white/70 sm:mt-6">
-					Sorry, we couldn’t find the page you’re looking for.
+					Désolé, une erreur est survenue. Veuillez réessayer.
+				</p>
+				<p className={'text-white/70'}>
+					Ou contactez-nous si le problème persiste.
 				</p>
 				<div className="mt-10 flex justify-center">
 					<button
@@ -33,7 +36,10 @@ export default function Error({ error, reset }) {
 							() => reset()
 						}
 					>
-						<span aria-hidden="true">&larr;</span> Back to home
+						<span aria-hidden="true" className={'text-white'}>
+							&larr;
+						</span>{' '}
+						Revenir en arrière
 					</button>
 				</div>
 			</div>
